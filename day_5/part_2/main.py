fbf0534b9da3096f197dc62b203a236be663a29b
Spansos <stan.spaans@student.hu.nl>
2023-12-05 12:34:45 +0100

    k

--- day_5/part_2/main.py
+++ day_5/part_2/main.py
@@ -17,80 +17,42 @@ for i, v in enumerate(input[1:]):
 
 thing_to_things = [ [(int(j.split(' ')[0]), int(j.split(' ')[1]), int(j.split(' ')[2])) for j in i] for i in thing_to_things ]
 
-def convert_number( n, convertinfo ):
+def overlap( range1, range2 ):
+    return ( max( range1[0], range2[0] ), min( range1[1], range2[1] ) )
+
+def next_type( rng, convertinfo ):
+    left = [ rng ]
+    r = []
+    for rng in left:
+        for ci in convertinfo:
+            
+        
     for ci in convertinfo:
-        if n >= ci[1] and n < ci[1] + ci[2]:
-            return n+ci[0]-ci[1]
-    return n
+        ovrlp = overlap( rng, (ci[1],ci[1]+ci[2]) )
+        delta = ci[0]-ci[1]
+        r.append( ( ovrlp[0]+delta, ovrlp[1]+delta ) )
+    if r:
+        return r
+    return rng
 
-def reverse_convert_number( n, i ):
-    convertinfo = thing_to_things[i]
-    for ci in convertinfo:
-        if n >= ci[0] and n < ci[0] + ci[2]:
-            return n+ci[1]-ci[0]
-    return n
+def min_location( range_, thing_index ):
+    if thing_index == len(thing_to_things):
+        return range_[0]
 
-def smallest_dest( convertinfo, dissallowedsources ):
-    sdest = -1
-    for ci in convertinfo:
-        for i in range(ci[2]):
-            dest = ci[0]+i
-            src = ci[1]+i
-            if dest < sdest or sdest == -1:
-                if not src in dissallowedsources:
-                    sdest = dest
-            else:
-                break
-    return sdest
+    locations = []
+    for ci in thing_to_things[thing_index]:
+        converted = convert( range_, ci )
+        if converted[0] >= converted[1]:
+            continue
+        locations.append( min_location( converted, thing_index+1 ) )
 
-def is_seed( n ):
-    for i, j in zip(seeds[::2], seeds[1::2]):
-        if n >= i and n < i+j:
-            return True
-    return False
+    return min( locations )
 
-sn = -1
-n = 1
-while not is_seed(n):
-    sn += 1
-    n = sn
-    for i in range(len(thing_to_things)-1, -1, -1):
-        n = reverse_convert_number( n, i )
-    if not sn % 100000:
-        print(sn)
-print(sn)
+seedranges = [(i, i+j) for i, j in zip(seeds[::2], seeds[1::2])]
+locations = []
+for seedrange in seedranges:
+    locations.append( min_location(seedrange, 0) )
+# print(locations)
+print(convert( (79, 79+14), thing_to_things[0][0] ))
 
-# disallowed = [set() for i in range(len(thing_to_things))]
-# while True:
-
-#     # smallest pos anwser and from which converted
-#     sdest, si = 100000000000000000, 0
-#     for i, v in enumerate(thing_to_things):
-#         dest = smallest_dest( v, disallowed[i] )
-#         if dest <= sdest:
-#             sdest = dest
-#             si = i
-#     print(sdest, si)
-    
-#     for i in range(si, -1, -1):
-#         # fuck it, sdest and ssrc switched names
-#         sdest, ssrc = reverse_convert_number( sdest, thing_to_things[i] ), sdest
-#         disallowed[i].add(sdest)
-#     # re-switched names
-#     sdest, ssrc = ssrc, sdest
-    
-#     # print(ssrc)
-
-#     print(disallowed)
-
-
-#     # break
-
-# # final_nums = []
-# # for x, y in zip(seeds[::2], seeds[1::2]):
-# #     for i in range(x, x+y):
-# #         n = i
-# #         for j in thing_to_things:
-# #             n = convert_number( n, j )
-# #         final_nums.append(n)
-# # print(min(final_nums))
+# print(convert((20, 30), (26, 22, 5)))
