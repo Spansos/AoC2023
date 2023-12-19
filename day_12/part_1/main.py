868434e66abb38fe07c13e2465b00b6ade25855d
Spans <spansos@Spansos.home>
2023-12-19 02:00:07 +0100

    bruh moment

--- day_12/part_1/main.py
+++ day_12/part_1/main.py
@@ -1,67 +1,103 @@
 with open("part_1/input.txt") as file:
     input = file.readlines()
 input = [i.strip() for i in input]
-arrangements, nums = [], []
-for i in input:
-    a, b = i.split(' ')
-    arrangements.append(a)
-    nums.append(b)
-nums = [tuple([int(k) for k in j.split(',')]) for j in nums]
+input = [i.split(' ') for i in input]
+input = [(i, tuple([int(j) for j in k.split(',')])) for i,k in input]
 
-arrangements = [tuple([j for j in i.split('.')]) for i in arrangements]
-brokens = []
-starts = []
-for i, ar in enumerate(arrangements):
-    brokens.append([])
-    starts.append([])
-    for j, l in enumerate(ar):
-        if l != '':
-            brokens[-1].append(l)
-            starts[-1].append(j+sum([len(k) for k in ar[:j]]))
-
-import functools
-
-
-@functools.cache
-def get_arrangements( brokens, nums, starts, start ):
-    if not nums and all([all([i!='#' for i in j]) for j in brokens]):
-        return [[]]
+def arrangements( arrangement, nums ):
+    if not nums and all([i!='#' for i in arrangement]):
+        return [arrangement]
     if not nums:
-        return None
-    arrangements = []
+        return []
+    r = []
     cur = nums[0]
-    for i, arrangement in enumerate(brokens):
-        curstart = starts[i]
-        for j, c in enumerate(arrangement):
-            if len(arrangement)-j >= cur:
-                if j+curstart < start:
-                    continue
-                if j-1 >= 0 and brokens[i][j-1] == '#':
-                    continue
-                if j+cur < len(brokens[i]) and brokens[i][j+cur] == '#':
-                    continue
-                part1 = brokens[i][:max(j-1,0)]
-                part2 = brokens[i][j+cur+1:]
-                nbrokens = (*brokens[:i], ) + ((part1,) if part1 else ()) + ((part2,) if part2 else ()) + (*brokens[i+1:], )
-                nstarts = []
-                if part1: nstarts.append(curstart)
-                if part2: nstarts.append(j+cur+curstart+1)
-                nstarts = tuple(nstarts+[v for p, v in enumerate(starts) if p != i])
-                r = get_arrangements( nbrokens, nums[1:], nstarts, curstart+cur+j )
-                if r:
-                    # print(brokens, nbrokens, repr(part1), repr(part2), cur,'|',curstart,starts, '|', i, j)
-                    for s in r:
-                        arrangements.append(list(range(j+curstart,j+cur+curstart)) + s)
-                # print( 'nbr:', nbrokens, 'brkns:',brokens,'nm:', nums, 'nstrt:', nstarts, 'strt:',starts,'ar:', arrangements,'crstrt:', curstart,'cr:', cur,'i:',i,'j:', j,'r:',r )
-                # else:
-                #     arrangements.append(list(range(j+curstart,j+cur+curstart)))
-    return arrangements
-    
+    for i, c in enumerate(arrangement):
+        if i+cur > len(arrangement):
+            continue
+        if i-1 >= 0 and arrangement[i-1] == '#':
+            continue
+        if i+cur < len(arrangement) and arrangement[i+cur] == '#':
+            continue
+        if any([j=='.' for j in arrangement[i:i+cur]]):
+            continue
+        narrangement = ''.join(['.' if j <= i+cur else k for j,k in enumerate(arrangement)])
+        for j in arrangements( narrangement, nums[1:] ):
+            r.append((j[:i] + '#'*cur + j[i+cur:]).replace('?', '.'))
+        if arrangement[i] == '#':
+            break
+    return r
+
 s = 0
-for i in range(len(brokens)):
-    fss = set([frozenset(i) for i in get_arrangements(tuple(brokens[i]), tuple(nums[i]), tuple(starts[i]), 0)])
-    s += len(fss)
+for ar, nums in input:
+    v = arrangements(ar, nums)
+    s += len(v)
 print(s)
+
+# v = arrangements('?###????????', (3,2,1))
+# for i in sorted(list(v)):
+#     print(i)
+
+# arrangements, nums = [], []
+# for i in input:
+#     a, b = i.split(' ')
+#     arrangements.append(a)
+#     nums.append(b)
+# nums = [tuple([int(k) for k in j.split(',')]) for j in nums]
+
+# arrangements = [tuple([j for j in i.split('.')]) for i in arrangements]
+# brokens = []
+# starts = []
+# for i, ar in enumerate(arrangements):
+#     brokens.append([])
+#     starts.append([])
+#     for j, l in enumerate(ar):
+#         if l != '':
+#             brokens[-1].append(l)
+#             starts[-1].append(j+sum([len(k) for k in ar[:j]]))
+
+# import functools
+
+
+# @functools.cache
+# def get_arrangements( brokens, nums, starts, start ):
+#     if not nums and all([all([i!='#' for i in j]) for j in brokens]):
+#         return [[]]
+#     if not nums:
+#         return None
+#     arrangements = []
+#     cur = nums[0]
+#     for i, arrangement in enumerate(brokens):
+#         curstart = starts[i]
+#         for j, c in enumerate(arrangement):
+#             if len(arrangement)-j >= cur:
+#                 if j+curstart < start:
+#                     continue
+#                 if j-1 >= 0 and brokens[i][j-1] == '#':
+#                     continue
+#                 if j+cur < len(brokens[i]) and brokens[i][j+cur] == '#':
+#                     continue
+#                 part1 = brokens[i][:max(j-1,0)]
+#                 part2 = brokens[i][j+cur+1:]
+#                 nbrokens = (*brokens[:i], ) + ((part1,) if part1 else ()) + ((part2,) if part2 else ()) + (*brokens[i+1:], )
+#                 nstarts = []
+#                 if part1: nstarts.append(curstart)
+#                 if part2: nstarts.append(j+cur+curstart+1)
+#                 nstarts = tuple(nstarts+[v for p, v in enumerate(starts) if p != i])
+#                 r = get_arrangements( nbrokens, nums[1:], nstarts, curstart+cur+j )
+#                 if r:
+#                     # print(brokens, nbrokens, repr(part1), repr(part2), cur,'|',curstart,starts, '|', i, j)
+#                     for s in r:
+#                         arrangements.append(list(range(j+curstart,j+cur+curstart)) + s)
+#                 # print( 'nbr:', nbrokens, 'brkns:',brokens,'nm:', nums, 'nstrt:', nstarts, 'strt:',starts,'ar:', arrangements,'crstrt:', curstart,'cr:', cur,'i:',i,'j:', j,'r:',r )
+#                 # else:
+#                 #     arrangements.append(list(range(j+curstart,j+cur+curstart)))
+#     return arrangements
+    
+# s = 0
+# for i in range(len(brokens)):
+#     fss = set([frozenset(i) for i in get_arrangements(tuple(brokens[i]), tuple(nums[i]), tuple(starts[i]), 0)])
+#     s += len(fss)
+# print(s)
 # fss = set([frozenset(j) for j in get_arrangements(tuple(('????',)), tuple((1,)), tuple((0,)))])
 # print(brokens[i], nums[i], starts[i])
 # print(fss, len(fss))
